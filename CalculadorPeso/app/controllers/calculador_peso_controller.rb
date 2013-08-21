9ddd5bc18c5f26bc48572348b70d8e0e93751569
sarboleda609 <tatan2008@gmail.com>
2013-08-21 14:13:45 -0500

    Update calculador_peso_controller.rb
    Agrego formula Miller

--- CalculadorPeso/app/controllers/calculador_peso_controller.rb
+++ CalculadorPeso/app/controllers/calculador_peso_controller.rb
@@ -13,28 +13,28 @@ class CalculadorPesoController < ApplicationController
             @result = "femenino kg BMI cm " + params[:alt]
           elsif params[:met].to_f == 2
 
-            @result = (100.1 + 5.06*((params[:alt].to_f*					0.393700787)-60))*0.45359237
+            @result = (100.1 + 5.06*((params[:alt].to_f*0.393700787)-60))*0.45359237
 
           elsif params[:met].to_f == 3
 
-            @result = (107.8 + 3.74*((params[:alt].to_f*					0.393700787)-60))*0.45359237
+            @result = (107.8 + 3.74*((params[:alt].to_f*0.393700787)-60))*0.45359237
 
           elsif params[:met].to_f == 4
-            @result = "femenino kg Miller cm " + params[:alt]
+            @result = (116.82 + 2.99*((params[:alt].to_f*0.393700787)-60))*0.45359237
           end
         elsif params[:unidadr].to_f == 2
           if params[:met].to_f == 1
             @result = "femenino Lb BMI cm " + params[:alt]
           elsif params[:met].to_f == 2
 
-            @result = (100.1 + 5.06*((params[:alt].to_f*					0.393700787)-60))
+            @result = (100.1 + 5.06*((params[:alt].to_f*0.393700787)-60))
 
           elsif params[:met].to_f == 3
 
-            @result = (107.8 + 3.74*((params[:alt].to_f*					0.393700787)-60))
+            @result = (107.8 + 3.74*((params[:alt].to_f*0.393700787)-60))
 
           elsif params[:met].to_f == 4
-            @result = "femenino Lb Miller cm " + params[:alt]
+            @result = (116.82 + 2.99*((params[:alt].to_f*0.393700787)-60))
           end
         end
       elsif params[:unidadi].to_f == 2
@@ -44,28 +44,28 @@ class CalculadorPesoController < ApplicationController
             @result = "Masculino kg BMI cm " + params[:alt]
           elsif params[:met].to_f == 2
 
-            @result = (110 + 5.06*((params[:alt].to_f*					0.3937007870787)-60))*0.45359237
+            @result = (110 + 5.06*((params[:alt].to_f*0.3937007870787)-60))*0.45359237
 
           elsif params[:met].to_f == 3
 
-            @result = (114.4 + 4.18*((params[:alt].to_f*					0.393700787)-60))*0.45359237
+            @result = (114.4 + 4.18*((params[:alt].to_f*0.393700787)-60))*0.45359237
 
           elsif params[:met].to_f == 4
-            @result = "Masculino kg Miller cm " + params[:alt]
+            @result = (123.64 + 3.10*((params[:alt].to_f*0.393700787)-60))*0.45359237
           end
         elsif params[:unidadr].to_f == 2
           if params[:met].to_f == 1
             @result = "Masculino Lb BMI cm " + params[:alt]
           elsif params[:met].to_f == 2
 
-            @result = (110 + 5.06*((params[:alt].to_f*					0.393700787)-60))
+            @result = (110 + 5.06*((params[:alt].to_f*0.393700787)-60))
 
           elsif params[:met].to_f == 3
 
-            @result = (114.4 + 4.18*((params[:alt].to_f*					0.393700787)-60))
+            @result = (114.4 + 4.18*((params[:alt].to_f*0.393700787)-60))
 
           elsif params[:met].to_f == 4
-            @result = "Masculino Lb Miller cm " + params[:alt]
+            @result = (123.64 + 3.10*((params[:alt].to_f*0.393700787)-60))
           end
        
         end
@@ -82,10 +82,10 @@ class CalculadorPesoController < ApplicationController
 
           elsif params[:met].to_f == 3
 
-            @result = (107.8 + 3.74*((params[:alt].to_f)-60))*				0.45359237
+            @result = (107.8 + 3.74*((params[:alt].to_f)-60))*0.45359237
 
           elsif params[:met].to_f == 4
-            @result = "femenino kg Miller pulg " + params[:alt]
+            @result = (116.82 + 2.99*((params[:alt].to_f)-60))*0.45359237
           end
         elsif params[:unidadr].to_f == 2
           if params[:met].to_f == 1
@@ -99,7 +99,7 @@ class CalculadorPesoController < ApplicationController
             @result = (107.8 + 3.74*((params[:alt].to_f)-60))
 
           elsif params[:met].to_f == 4
-            @result = "femenino Lb Miller pulg " + params[:alt]
+            @result = (116.82 + 2.99*((params[:alt].to_f)-60))
           end
         end
       elsif params[:unidadi].to_f == 2
@@ -109,14 +109,14 @@ class CalculadorPesoController < ApplicationController
             @result = "Masculino kg BMI pulg " + params[:alt]
           elsif params[:met].to_f == 2
 
-            @result = (110 + 5.06*((params[:alt].to_f)-60))*				0.45359237
+            @result = (110 + 5.06*((params[:alt].to_f)-60))*0.45359237
 
           elsif params[:met].to_f == 3
 
-            @result = (114.4 + 4.18*((params[:alt].to_f)-60))*				0.45359237
+            @result = (114.4 + 4.18*((params[:alt].to_f)-60))*0.45359237
 
           elsif params[:met].to_f == 4
-            @result = "Masculino kg Miller pulg " + params[:alt]
+            @result = (123.82 + 3.10*((params[:alt].to_f)-60))*0.45359237
           end
         elsif params[:unidadr].to_f == 2
           if params[:met].to_f == 1
@@ -130,7 +130,7 @@ class CalculadorPesoController < ApplicationController
             @result = (114.4 + 4.18*((params[:alt].to_f)-60))
 
           elsif params[:met].to_f == 4
-            @result = "Masculino Lb Miller pulg " + params[:alt]
+            @result = (123.82 + 3.10*((params[:alt].to_f)-60))
           end
         end
       end
